075328b1eded4a2c36980c89e85434270ca1d60f
vishnu <vishnu.v>
2021-08-18 19:16:56 +0530

    code change

---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+Public class Main {
+	public static void Main (string[] args) {
+		int my age=15;
+		double gpa=8.0;
+		boolean attendance perfect =true;
+		system.out.println(my age);
+		system.out.println(gpa);
+		system.out.printlan(Attendance perfect);
+	}
+}
